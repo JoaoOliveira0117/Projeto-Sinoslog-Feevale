e512ad5cbda0c9cdba74e318825d5c5b4157f829
FehuKs <fehubners@gmail.com>
2024-05-01 16:55:52 -0300

    Modificando a classe NumerosDeContato
    Adicionada funcionalidade de exibição da lista de contatos e permitindo ao usuário fazer ligações a partir do aplicativo

--- app/src/main/java/com/example/projetofeevale/helpers/NumerosDeContato.java
+++ app/src/main/java/com/example/projetofeevale/helpers/NumerosDeContato.java
@@ -1,18 +1,14 @@
 package com.example.projetofeevale.helpers;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.annotation.SuppressLint;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.WindowInsets;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-
-import com.example.projetofeevale.databinding.ActivityNumerosdeContatoBinding;
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import com.example.projetofeevale.R;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -20,159 +16,59 @@ import com.example.projetofeevale.databinding.ActivityNumerosdeContatoBinding;
  */
 public class NumerosDeContato extends AppCompatActivity {
 
-    private ActivityNumerosdeContatoBinding binding;
-    private TextView fullscreenContent;
-    private LinearLayout fullscreenContentControls;
-    private final Handler hideHandler = new Handler(Looper.myLooper());
-
-    @SuppressLint("InlinedApi")
-    private final Runnable hidePart2Runnable = new Runnable() {
-        @Override
-        public void run() {
-            // Delayed removal of status and navigation bar
-            if (Build.VERSION.SDK_INT >= 30) {
-                fullscreenContent.getWindowInsetsController().hide(WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
-            } else {
-                // Note that some of these constants are new as of API 16 (Jelly Bean)
-                // and API 19 (KitKat). It is safe to use them, as they are inlined
-                // at compile-time and do nothing on earlier devices.
-                fullscreenContent.setSystemUiVisibility(
-                        View.SYSTEM_UI_FLAG_LOW_PROFILE |
-                                View.SYSTEM_UI_FLAG_FULLSCREEN |
-                                View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
-                                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
-                                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
-                                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-            }
-        }
-    };
-    private final Runnable showPart2Runnable = new Runnable() {
-        @Override
-        public void run() {
-            // Delayed display of UI elements
-            //getSupportActionBar().show();
-            fullscreenContentControls.setVisibility(View.VISIBLE);
-        }
-    };
-    private boolean isFullscreen = false;
-
-    private final Runnable hideRunnable = new Runnable() {
-        @Override
-        public void run() {
-            hide();
-        }
-    };
-
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    private final View.OnTouchListener delayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            switch (motionEvent.getAction()) {
-                case MotionEvent.ACTION_DOWN:
-                    if (AUTO_HIDE) {
-                        delayedHide(AUTO_HIDE_DELAY_MILLIS);
-                    }
-                    break;
-                case MotionEvent.ACTION_UP:
-                    view.performClick();
-                    break;
-                default:
-                    break;
-            }
-            return false;
-        }
-    };
-
-    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_numerosde_contato);
 
-        binding = ActivityNumerosdeContatoBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        // Lista de contatos fixa
+        final String[] contacts = {"Bombeiros","Brigada Militar", "Disque Denúncia","Disque Saúde",
+                "Canil Municipal",
+                "COMUSA", "Defesa Civil", "Guarda Municipal","RGE Sul"};
 
-        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, contacts);
+        ListView listView = findViewById(R.id.listView);
+        listView.setAdapter(adapter);
 
-        isFullscreen = true;
-
-        // Set up the user interaction to manually show or hide the system UI.
-        fullscreenContent = binding.fullscreenContent;
-        fullscreenContent.setOnClickListener(new View.OnClickListener() {
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                toggle();
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String contactName = contacts[position];
+                String contactNumber = getContactNumber(contactName); // Obtenha o número de contato correspondente ao nome do contato
+                if (contactNumber != null) {
+                    Intent intent = new Intent(Intent.ACTION_DIAL);
+                    intent.setData(Uri.parse("tel:" + contactNumber));
+                    startActivity(intent);
+                }
             }
         });
-
-        fullscreenContentControls = binding.fullscreenContentControls;
-
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        binding.dummyButton.setOnTouchListener(delayHideTouchListener);
     }
 
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
-        delayedHide(100);
-    }
-
-    private void toggle() {
-        if (isFullscreen) {
-            hide();
-        } else {
-            show();
+    // Método para obter o número de contato correspondente ao nome do contato
+    private String getContactNumber(String contactName) {
+        // Aqui você pode implementar lógica para mapear o nome do contato para o número de contato desejado
+        // Por simplicidade, este exemplo apenas retorna um número fixo para cada contato
+        switch (contactName) {
+            case "Bombeiros":
+                return "35951123";
+            case "Brigada Militar":
+                return "190";
+            case "Canil Municipal 2":
+                return "996832117";
+            case "COMUSA":
+                return "0800 6000 115";
+            case "Defesa Civil":
+                return "30979408";
+            case "Disque Denúncia":
+                return "32885100";
+            case "Disque Saúde":
+                return "0800 611997";
+            case "Guarda Municipal":
+                return "35248737";
+            case "RGE Sul":
+                return "08007077272";
+            default:
+                return null;
         }
     }
-
-    private void hide() {
-        // Hide UI first
-        //getSupportActionBar().hide();
-        fullscreenContentControls.setVisibility(View.GONE);
-        isFullscreen = false;
-
-        // Schedule a runnable to remove the status and navigation bar after a delay
-        hideHandler.removeCallbacks(showPart2Runnable);
-        hideHandler.postDelayed(hidePart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    private void show() {
-        // Show the system bar
-        if (Build.VERSION.SDK_INT >= 30) {
-            fullscreenContent.getWindowInsetsController().show(WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
-        } else {
-            fullscreenContent.setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
-                            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-        }
-        isFullscreen = true;
-
-        // Schedule a runnable to display UI elements after a delay
-        hideHandler.removeCallbacks(hidePart2Runnable);
-        hideHandler.postDelayed(showPart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    /**
-     * Schedules a call to hide() in [delayMillis], canceling any
-     * previously scheduled calls.
-     */
-    private void delayedHide(int delayMillis) {
-        hideHandler.removeCallbacks(hideRunnable);
-        hideHandler.postDelayed(hideRunnable, delayMillis);
-    }
-
-    private static final boolean AUTO_HIDE = true;
-
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-
-    private static final int UI_ANIMATION_DELAY = 300;
 }
