62516961a8ff160ff37f0093b71056b7f5109143
João Oliveira <joao.oliveira0117@hotmail.com>
2024-05-06 22:34:51 -0300

    add google maps api key

---
+++ app/src/main/java/com/example/projetofeevale/activities/MapViewFragment.java
@@ -0,0 +1,91 @@
+package com.example.projetofeevale.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.projetofeevale.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link MapViewFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MapViewFragment extends Fragment implements OnMapReadyCallback {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    private GoogleMap googleMap;
+    private SupportMapFragment mapFragment;
+
+    public MapViewFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment MapViewFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static MapViewFragment newInstance(String param1, String param2) {
+        MapViewFragment fragment = new MapViewFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        mapFragment = new SupportMapFragment();
+        getParentFragmentManager().beginTransaction().replace(R.id.googleMap, mapFragment).commit();
+        mapFragment.getMapAsync(this);
+
+        return inflater.inflate(R.layout.fragment_map_view, container, false);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        this.googleMap = googleMap;
+
+        LatLng campoBom = new LatLng(-29.6760811, -51.0907438);
+        this.googleMap.addMarker(new MarkerOptions().position(campoBom));
+        this.googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(campoBom, 14));
+    }
+}
